d4d759aa972c46e702ba0960f1d2ed5724290b75
F-balanta <jf.balanta@hotmail.com>
2021-06-24 02:07:28 +0000

    Create files and fixed files with Betty style

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 #include "holberton.h"
@@ -9,18 +10,17 @@
 void positive_or_negative(int i)
 {
 
-	srand(time(0));
-	i = rand() - RAND_MAX / 2;
-	if (i > 0)
-	{
-		printf("%d is positive\n", i);
-	}
-	else if (i == 0)
-	{
-		printf("%d is zero\n", i);
-	}
-	else
-	{
-		printf("%d is zero\n", i);
-	}
+srand(time(0));
+i = rand() - RAND_MAX / 2;
+if (i > 0)
+{
+printf("%d is positive\n", i);
+}
+else if
+(i == 0)
+printf("%d is zero\n", i);
+else
+{
+printf("%d is negative\n", i);
+}
 }
