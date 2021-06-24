85d222b9e2fd8241c79a145fb25863c158b1b340
F-balanta <jf.balanta@hotmail.com>
2021-06-24 01:58:26 +0000

    files debugging

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+#include "holberton.h"
+
+/**
+ * main - is a principal function
+ * Return: always 0
+ */
+void positive_or_negative(int i)
+{
+
+	srand(time(0));
+	i = rand() - RAND_MAX / 2;
+	if (i > 0)
+	{
+		printf("%d is positive\n", i);
+	}
+	else if(i == 0)
+	{
+		printf("%d is zero\n", i);
+	}
+	else
+	{
+		printf("%d is zero\n", i);
+	}
+}
