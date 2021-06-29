3e546750859abbe6e427c1c6ab563591b72dc29d
F-balanta <jf.balanta@hotmail.com>
2021-06-29 22:21:39 +0000

    solving activities on about pointers

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+/**
+ * _strlen - function that returns the length of a string.
+ * @s:external input
+ * Return:int
+ */
+#include "holberton.h"
+int _strlen(char *s)
+{
+	int j  = 0;
+
+	while (s[j] != '\0')
+		j++;
+	return (j);
+}
